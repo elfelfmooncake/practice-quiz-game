4ad74a55e8ee99c537484b30a779c123efb888bf
Elva Fan <Elfelf_mooncake@Elvas-MacBook-Air.local>
2015-01-05 21:58:32 -0800

    add initial python files

---
+++ mystery_lover_game.py
@@ -0,0 +1,49 @@
+import time
+import sys
+
+
+print "Hello! This is a game to tell who your mystery lover is!"
+
+time.sleep(1.5)
+
+# this variable will +1 each time player scores correctly #
+cumulative_score = 1
+
+def graceful_exit(): 
+	print "This is your score: " + str(cumulative_score)
+	sys.exit()
+
+print "Are you ready?"
+ready_statement = raw_input("Type yes or no: ")
+
+if ready_statement == "no":
+	print "Nevermind! Didn't think you got the guts either. I'll give you a point for participation."
+	graceful_exit()
+elif ready_statement != "yes":
+	print "no, I don't understand what you said. Aliens are not allowed to participate here. Goodbye."
+	graceful_exit()
+
+print "Here's a challenge question: is 2 X 4 X 6 X 8...X 100 +1 divisible by a prime number under 50?"
+challenge1_answer = raw_input("Type yes or no: ")
+
+if challenge1_answer == "yes":
+	print "Ah.. I see math is not your forte... It's okay... we can go on"
+elif challenge1_answer == "no":
+	print "You got your divisibility down. That's important to filter down to the right list of lovers. +1 point"
+	cumulative_score += 1
+else:
+	print "I see you're not serious about this game. Do you want to exit?"
+	time.sleep(1.5)
+	ready_statement = raw_input("Type yes or no: ")
+	if ready_statement == "no":
+		print "Nevermind! Didn't think you got the guts either. I'll give you a point for participation."
+		cumulative_score += 1
+		graceful_exit()
+	elif ready_statement != "yes":
+		print "no, I don't understand what you said. Aliens are not allowed to participate here. Goodbye."
+		cumulative_score += 1
+		graceful_exit()
+
+print "I'm sleepy and will continue making this game tomorrow. More contents coming!"
+time.sleep(1.5)
+graceful_exit()
---
+++ practice.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print "hello world"
+x = 2 
+if x == 1:
+	print "GMAT is math intensive"
+	print "Jeff is so sexy! 我是elva"
+else:
+	print abs(3.1415292653579396) 
+
+words = ["pipi", "manko", "chinchin"]
+for a in words:
+	print a, len(a)
+
+print len(words)
+
+if len(words) >= 3:
+	words.extend(["houhi", "shouben"])
+
+print words
+
